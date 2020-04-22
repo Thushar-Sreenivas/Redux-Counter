555e959bbbbf7bed16fdce388c60a1ca72959a3a
Thushar Sreenivas <Thushar-Sreenivas@users.noreply.github.com>
2020-04-22 14:20:19 +0530

    Dispatching action from component

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,11 @@ const intialState = {
     counter: 0
 }
 const reducer = (state = intialState, action) => {
+    if(action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1            
+        }
+    }
     return state
 }
 export default reducer
